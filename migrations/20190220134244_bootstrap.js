f45ec7389432196625f21453b84c9d4bac21b03b
croy <roy.christie95@gmail.com>
2019-02-20 13:52:20 -0500

    Cohorts table successfully created and shows up in SQLite

--- migrations/20190220134244_bootstrap.js
+++ migrations/20190220134244_bootstrap.js
@@ -3,12 +3,11 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('cohorts', function(tbl) {
     // Autogenerates the primary key which auto-increments  
     tbl.increments();
-
     tbl.string('name', 255).notNullable();
   });
 };
 
 // How can I undo the changes? Undoes the structure, not the data
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExist('cohorts');
 };
